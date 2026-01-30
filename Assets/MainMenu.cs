ab684a65a902c8fe1068a9515505ac700c3cbf44
Stepan Seleznev <stevesttxd@gmail.com>
2026-01-30 20:18:08 +0100

    m

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("DayTimeScene");
     }
 
     public void Settings()
