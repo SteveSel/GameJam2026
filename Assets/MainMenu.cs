e5650e957e6322c30014a96029f7828327d28953
Stepan Seleznev <stevesttxd@gmail.com>
2026-01-31 18:07:58 +0100

    m

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,12 +5,32 @@ public class MainMenu : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene("NightTimeScene");
+        SceneManager.LoadScene("modeSelection");
     }
 
     public void Settings()
     {
         SceneManager.LoadScene("Settings");
+    }
+
+    public void LevelSelector()
+    {
+        SceneManager.LoadScene("LevelSelector");
+    }
+
+    public void Custom()
+    {
+        SceneManager.LoadScene("Custom1");
+    }
+
+    public void MainPlay()
+    {
+        SceneManager.LoadScene("NightTimeScene");
+    }
+
+    public void Load() 
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void Salir()
